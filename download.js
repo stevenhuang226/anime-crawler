5871c5704f7a5b6d28b0ae0b027892734349b3a6
stevenhuang226 <stevenhuang.dog@gmail.com>
2024-05-16 00:13:39 +0800

    coding downloader

--- download.js
+++ download.js
@@ -58,3 +58,44 @@ async function typeOfDownload(theUrl, siteType, fileName) {
 	console.log("Error Unknow Site Type");
 	return -1;
 }
+async function mySelf(theUrl, fileName) {
+	const https = require("https");
+	const url = require("url");
+	return new Promise( (resolve,reject) => {
+		const reqObj = {
+			hostname: url.parse(theUrl).hostname,
+			path: url.parse(theUrl).path,
+			method: "GET",
+			headers: {
+				"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:125.0) Gecko/20100101 Firefox/125.0",
+				"Accept": "*/*",
+				"Accept-Language": "en-US,en;q=0.5",
+				"Referer": "https://v.myself-bbs.com/",
+				"Origin": "https://v.myself-bbs.com",
+				"Connection": "keep-alive",
+				"Sec-Fetch-Dest": "empty",
+				"Sec-Fetch-Mode": "no-cors",
+				"Sec-Fetch-Site": "same-site",
+				"Pragma": "no-cache",
+				"Cache-Control": "no-cache",
+			};
+		};
+		const reqBody = https.request(reqObj, (response) => {
+			let originData = "";
+			response.on("data", (data) => {
+				originData += data;
+			});
+			response.on("end", () => {
+				resolve(originData);
+			});
+		});
+		reqBody.on("error", (error) => {
+			reject(error);
+		});
+		reqBody.end();
+	}).then( (m3u8Url) => {
+		//TODO parse m3u8 file
+	}).catch( (error) => {
+		console.log(error);
+	} )
+}
